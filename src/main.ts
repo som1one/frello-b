8a1d2336198134a0e5adb68d97f0c0a361625555
elbek <elbek4561@gmail.com>
2025-11-16 18:20:41 +0500

    removed port

--- src/main.ts
+++ src/main.ts
@@ -1,30 +1,26 @@
-import { Logger } from '@nestjs/common'
-import { NestFactory } from '@nestjs/core'
-import * as cookieParser from 'cookie-parser'
-import * as dotenv from 'dotenv'
+import { NestFactory } from "@nestjs/core";
+import * as cookieParser from "cookie-parser";
 
-import { AppModule } from './app.module'
-import { AllExceptionsFilter } from './common/filters/all-exceptions.filter'
+import { AppModule } from "./app.module";
+import { Logger } from "@nestjs/common";
+import { AllExceptionsFilter } from "./common/filters/all-exceptions.filter";
 
 async function bootstrap() {
-	dotenv.config() // <-- .env faylni yuklaymiz
+  const app = await NestFactory.create(AppModule, {
+    logger: ["log", "error", "warn", "debug", "verbose"],
+  });
+  // logger: ['error', 'warn', 'log']
+  app.useGlobalFilters(new AllExceptionsFilter());
+  app.setGlobalPrefix("api");
+  app.use(cookieParser());
+  app.enableCors({
+    origin: ["https://frello.ru/", "https://frello.ru/"],
+    credentials: true,
+    exposedHeaders: "set-cookie",
+  });
 
-	const app = await NestFactory.create(AppModule, {
-		logger: ['log', 'error', 'warn', 'debug', 'verbose']
-	})
-	// logger: ['error', 'warn', 'log']
-	app.useGlobalFilters(new AllExceptionsFilter())
-	app.setGlobalPrefix('api')
-	app.use(cookieParser())
-	app.enableCors({
-		origin: ['http://localhost:3000', 'http://localhost:3001'],
-		credentials: true,
-		exposedHeaders: 'set-cookie'
-	})
+  Logger.log("Application started", "Bootstrap");
 
-	Logger.log('Application started', 'Bootstrap')
-
-	const port = process.env.PORT || 4200 // <-- .env fayldagi PORT ishlaydi, default 4200
-	await app.listen(port)
+  await app.listen(4200);
 }
-bootstrap()
+bootstrap();
